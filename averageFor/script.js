3e0f862333d1147c3ca72f35b64b09b1b6195929
biji03 <bijan.babarahimi@gmail.com>
2023-01-23 14:28:26 +0330

    feat:avrage for

---
+++ averageFor/script.js
@@ -0,0 +1,7 @@
+var finalPrices = 0
+
+for (price = 0; price < 5; price++) {
+    finalPrices = finalPrices + Number(prompt("please enter The price: "))
+}
+
+alert("Your price of your shoping: " + finalPrices)
